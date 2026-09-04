73024dc6e65327c16ee89844603f06901c50239e
Tom Gardner <103318430+thomasGardner19@users.noreply.github.com>
2026-09-04 14:26:37 -0400

    Fix Census package lint ordering

--- src/nh_property_intelligence/ingestion/census/__init__.py
+++ src/nh_property_intelligence/ingestion/census/__init__.py
@@ -1,14 +1,14 @@
 """Census ACS ingestion support."""
 
-from .contract import ACS_VARIABLES, RawMunicipalityRow, RunContext
 from .client import RequestSpec, build_request, fetch_response
+from .contract import ACS_VARIABLES, RawMunicipalityRow, RunContext
 from .normalize import normalize_response
 
 __all__ = [
     "ACS_VARIABLES",
     "RawMunicipalityRow",
-    "RunContext",
     "RequestSpec",
+    "RunContext",
     "build_request",
     "fetch_response",
     "normalize_response",
