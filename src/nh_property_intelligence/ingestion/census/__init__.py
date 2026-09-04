fa7280e746fa5e0dc9c34688a122fd9122e567ba
Tom Gardner <103318430+thomasGardner19@users.noreply.github.com>
2026-09-04 14:41:24 -0400

    Export Census Snowflake loader

--- src/nh_property_intelligence/ingestion/census/__init__.py
+++ src/nh_property_intelligence/ingestion/census/__init__.py
@@ -2,14 +2,17 @@
 
 from .client import RequestSpec, build_request, fetch_response
 from .contract import ACS_VARIABLES, RawMunicipalityRow, RunContext
+from .loader import LoadResult, replace_vintage
 from .normalize import normalize_response
 
 __all__ = [
     "ACS_VARIABLES",
+    "LoadResult",
     "RawMunicipalityRow",
     "RequestSpec",
     "RunContext",
     "build_request",
     "fetch_response",
     "normalize_response",
+    "replace_vintage",
 ]
