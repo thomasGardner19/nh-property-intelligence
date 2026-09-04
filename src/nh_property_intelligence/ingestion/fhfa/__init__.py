1b0c0afa072a32270c416359d36ee87db8cb4155
Tom Gardner <103318430+thomasGardner19@users.noreply.github.com>
2026-09-04 16:06:59 -0400

    Fix FHFA package lint

--- src/nh_property_intelligence/ingestion/fhfa/__init__.py
+++ src/nh_property_intelligence/ingestion/fhfa/__init__.py
@@ -1,10 +1,17 @@
 """FHFA county HPI ingestion package."""
+
 from .client import fetch_workbook
 from .contract import RawCountyHpiRow, RunContext
 from .extract import extract_records
 from .loader import LoadResult, replace_all
 from .normalize import normalize_records
+
 __all__ = [
-    "LoadResult", "RawCountyHpiRow", "RunContext", "extract_records", "fetch_workbook",
-    "normalize_records", "replace_all",
+    "LoadResult",
+    "RawCountyHpiRow",
+    "RunContext",
+    "extract_records",
+    "fetch_workbook",
+    "normalize_records",
+    "replace_all",
 ]
